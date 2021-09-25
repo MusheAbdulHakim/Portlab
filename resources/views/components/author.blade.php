19b4a90f745a145dfacf33dd647fc66ccb44a95d
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-09-25 14:18:20 +0000

    article author

---
+++ resources/views/components/author.blade.php
@@ -0,0 +1,24 @@
+@props([])
+
+<div class="author-img">
+    <a href="#"><img src="{{asset('img/comment-2.jpg')}}" alt="author" /></a>
+    <ul class="author-social">
+      <li>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+      </li>
+      <li>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+      </li>
+      <li>
+        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+      </li>
+    </ul>
+  </div>
+  <div class="author-content">
+    <h4>Mushe Abdul-Hakim <a href="#">https://codewithmushe.tech/</a></h4>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+      ab aperiam corrupti maiores animi nisi ratione maxime quae in
+      doloremque corporis tempore earum ut voluptas exercitationem.
+    </p>
+  </div>
